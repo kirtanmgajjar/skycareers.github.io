6e75635ca736049879f51cf5a602b61e03204594
kirtanmgajjar <67625085+kirtanmgajjar@users.noreply.github.com>
2023-02-26 22:51:33 +0530

    Add files via upload

---
+++ static/login.js
@@ -0,0 +1,37 @@
+username = document.querySelector("#username");
+errormsg = document.querySelector(".errormessage");
+password = document.querySelector("#password");
+loginButton = document.querySelector("#login_button");
+emailCheck = /.[^()<>,;:"\\\[\]]+@+[a-zA-Z]+\.+[a-zA-z]/;
+//passwordCheck = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&.?]).{7,12}/;
+loginButton.disabled = true;
+
+username.addEventListener('input',toggle);
+password.addEventListener('input',toggle);
+
+
+function toggle(){
+    if(emailCheck.test(username.value) && password.value.length !=0 )
+        loginButton.disabled = false;
+    else
+        loginButton.disabled = true;
+}
+
+loginButton.addEventListener('click',function(event) {
+    event.preventDefault();
+    window.location.href = "../HTML pages/main.html";
+});
+
+username.addEventListener('input',() => {
+    if(emailCheck.test(username.value)){
+        errormsg.textContent = '';
+        errormsg.className = 'errormessage';
+    }
+    else {
+        errormsg.textContent = 'Please enter valid email.';
+        errormsg.className = 'errormessage active';
+    }
+});
+
+
+
