1d070c93647fb1b62a9e77649a1d7d1edf79c361
kirtanmgajjar <67625085+kirtanmgajjar@users.noreply.github.com>
2023-02-27 19:34:53 +0000

    .

--- static/registration.js
+++ static/registration.js
@@ -1,6 +1,26 @@
 password = document.querySelector("#password");
 pwdinfo = document.querySelector(".pswd_info");
 passwordCheck = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&.?]).{7,12}/;
-password.addEventListener("click",() => {
+
+password.addEventListener("focus",() => {
     pwdinfo.className = 'pswd_info active';
+});
+
+password.addEventListener("blur",() => {
+    pwdinfo.className = 'pswd_info';
+});
+
+password.addEventListener("input",() => {
+    numCheck = /(?=.*[0-9])/
+
+    switch (password.value) {
+        case 
+    }
+    if(numCheck.test(password.value))
+    {
+        document.getElementById("number").className = 'valid';
+    }
+    else{
+        document.getElementById("number").className = 'invalid';
+    }
 });
