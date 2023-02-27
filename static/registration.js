cf6478a0e9f65798f6237b8a9170184078669a31
kirtanmgajjar <67625085+kirtanmgajjar@users.noreply.github.com>
2023-02-27 19:57:12 +0000

    .

--- static/registration.js
+++ static/registration.js
@@ -12,15 +12,33 @@ password.addEventListener("blur",() => {
 
 password.addEventListener("input",() => {
     numCheck = /(?=.*[0-9])/
+    smallCaseCheck = /(?=.*[a-z])/
+    capitalCaseCheck = /(?=.*[A-Z])/
+    specialCheck = /(?=.*[*.!@$%^&.?])/
 
-    switch (password.value) {
-        case 
-    }
     if(numCheck.test(password.value))
-    {
         document.getElementById("number").className = 'valid';
-    }
-    else{
+    else
         document.getElementById("number").className = 'invalid';
-    }
+
+    if(smallCaseCheck.test(password.value))
+        document.getElementById("letter").className = 'valid';
+    else
+        document.getElementById("letter").className = 'invalid';
+
+    if(capitalCaseCheck.test(password.value))
+        document.getElementById("capital").className = 'valid';
+    else
+        document.getElementById("capital").className = 'invalid';
+
+    if(specialCheck.test(password.value))
+        document.getElementById("special").className = 'valid';
+    else
+        document.getElementById("special").className = 'invalid';
+
+    if(password.value.length>7 && password.value.length<15)
+        document.getElementById("length").className = 'valid';
+    else
+        document.getElementById("length").className = 'invalid';
+
 });
