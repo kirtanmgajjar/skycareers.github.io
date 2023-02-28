af8053d4f03abf55cdea78a5337a6825cb3ad060
Lord-Shad0w <122289539+Lord-Shad0w@users.noreply.github.com>
2023-02-28 20:18:18 +0000

    .

--- static/login.js
+++ static/login.js
@@ -1,41 +1,67 @@
-username = document.querySelector("#username");
-errormsg = document.querySelector(".errormessage");
-password = document.querySelector("#password");
+showpswd = document.querySelector("#eye-off");
+pwd = document.querySelector("#password");
+email = document.querySelector("#email");
 loginButton = document.querySelector("#login_button");
 
-emailCheck = /.[^()<>,;:"\\\[\]]+@+[a-zA-Z]+\.+[a-zA-z]/;
-//passwordCheck = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&.?]).{7,12}/;
 loginButton.disabled = true;
 
-username.addEventListener('input',toggle);
-password.addEventListener('input',toggle);
-
-
-function toggle(){
-    if(emailCheck.test(username.value) && password.value)
-        loginButton.disabled = false;
-    else
-        loginButton.disabled = true;
-}
-
-
-username.addEventListener('input',() => {
-    if(emailCheck.test(username.value)){
-        errormsg.textContent = '';
-        errormsg.className = 'errormessage';
+emailCheck = /^.[^@()<>,;:"\\\[\]]+@+[a-zA-Z]+\.+.*[a-zA-Z]$/;
+showpswd.addEventListener("click",() => {
+    if(showpswd.name == "eye-off-outline")
+    {
+        showpswd.name = 'eye-outline';
+        pwd.type = 'text';
     }
-    else {
-        errormsg.textContent = 'Please enter valid email.';
-        errormsg.className = 'errormessage active';
+    else
+    {
+        showpswd.name = 'eye-off-outline';
+        pwd.type = 'password';
     }
 });
 
-function showPass() {
-  if (password.type === "password") {
-    password.type = "text";
-  } else {
-    password.type = "password";
-  }
+
+email.addEventListener("focusout", styleset);
+email.addEventListener("focusin", styleset2);
+pwd.addEventListener("focusout", styleset);
+pwd.addEventListener("focusin", styleset2);
+
+email.addEventListener("input", () => {
+    if(emailCheck.test(email.value))
+    {
+        email.style.color = 'white';
+        email.nextElementSibling.style.color = 'white';
+        email.previousElementSibling.style.color = 'white';
+        email.parentElement.style.borderBottomColor = 'white';
+        
+    }
+    else
+    {
+        email.style.color = 'red';
+        email.nextElementSibling.style.color = 'red';
+        email.previousElementSibling.style.color = 'red';
+        email.parentElement.style.borderBottomColor = 'red';
+        
+    }
+    if(emailCheck.test(email.value)&&pwd.value)
+        loginButton.disabled = false;
+    else
+        loginButton.disabled = true;
+});
+
+pwd.addEventListener("input", () => {
+    if(emailCheck.test(email.value)&&pwd.value)
+        loginButton.disabled = false;
+    else
+        loginButton.disabled = true;
+});
+function styleset()
+{
+    if(this.value)
+        this.nextElementSibling.style.top = '-5px';
+    else
+        this.nextElementSibling.style.top = '50%';
 }
-
-
+function styleset2()
+{
+    this.nextElementSibling.style.top  = '-5px';
+}
