68d182ae3678217077913abb1d085679121f9dc9
kirtanmgajjar <67625085+kirtanmgajjar@users.noreply.github.com>
2023-03-01 21:23:28 +0000

    .

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import sqlite3
+import random
+import string
 from  flask import Flask, render_template,request,redirect, url_for,session
 from flask_session import Session
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+resumeFolder = os.path.join(app.instance_path, 'resume_uploads')
+os.makedirs(resumeFolder,exist_ok=True)
 
 @app.route("/")
 def index():
@@ -53,10 +59,15 @@ def register():
         pnumber = request.form['pnumber']
         email = request.form['email']
         password = request.form['password']
+        country = request.form['country']
+        resumeFile = request.files['file']
+        """resumeName = "".join((random.choice(string.ascii_letters)) for x in range(15))
+        resumeName+=".pdf" """
+        resumeFile.save(resumeFile.filename)
         conn = sqlite3.connect('./static/login.db')
         db = conn.cursor()
         db.execute("insert into login_details values (?,?)",(email,password))
-        db.execute("insert into registration_details (email_id,first_name,last_name,phone_number,dob,gender) values(?,?,?,?,?,?);",(email,fname,lname,pnumber,dob,gender))
+        db.execute("insert into registration_details (email_id,first_name,last_name,phone_number,dob,gender,country,resume) values(?,?,?,?,?,?,?,?);",(email,fname,lname,pnumber,dob,gender,country,resumeFile.filename))
         conn.commit()
         conn.close()
         #return render_template("trial.html",a=fname,b=lname,c=gender,d=dob,e=email,f=password,g=pnumber)
